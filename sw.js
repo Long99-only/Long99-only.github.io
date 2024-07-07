baa4aebce5cb5f012802abc815c1b7527f21ed91
Long99-only <superlong9988@gmail.com>
2024-07-07 16:04:53 +0800

    Site updated: 2024-07-07 16:04:52

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720338567787";
+const VERSION = "1720339490112";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
